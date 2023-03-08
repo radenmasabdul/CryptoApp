63ebfd2aaaeab24922ff7d8c02b8c560fd88bf93
radenmasabdul <abdul.tamsis@gmail.com>
2023-03-08 16:35:26 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Content from "./components/Content";
 import ContentList from "./components/ContentList";
 import CryptoList from "./components/CryptoList";
 import UpFooter from "./components/UpFooter";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <ContentList />
       <CryptoList />
       <UpFooter />
+      <Footer />
     </>
   );
 };
